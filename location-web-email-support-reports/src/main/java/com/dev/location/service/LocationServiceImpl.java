35e457674465ed389e4f58ae7188ebc0b1707c45
jacobprajan <jjacobprajan@gmail.com>
2018-05-08 23:18:00 +0100

    Email Sending Demo and Reports Generation using JFreeCharts(Utility layer)

---
+++ location-web-email-support-reports/src/main/java/com/dev/location/service/LocationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.dev.location.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.location.entities.Location;
+import com.dev.location.repositories.LocationRepository;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+	
+	@Autowired
+	private LocationRepository locationRepository;
+	
+	@Override
+	public Location saveLocation(Location location) {
+		return locationRepository.save(location); 
+	}
+
+	@Override
+	public Location updateLocation(Location location) {
+		return locationRepository.save(location); 
+	}
+
+	@Override
+	public void deleteLocation(Location location) {
+		locationRepository.delete(location);
+	}
+
+	@Override
+	public Location getLocationById(int id) {
+		return locationRepository.findById(id).get();
+	}
+
+	@Override
+	public List<Location> getAllLocations() {
+		return locationRepository.findAll();
+	}
+	
+	public LocationRepository getLocationRepository() {
+		return locationRepository;
+	}
+
+	public void setLocationRepository(LocationRepository locationRepository) {
+		this.locationRepository = locationRepository;
+	}
+
+}
